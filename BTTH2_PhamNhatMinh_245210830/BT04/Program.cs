7c17da2f2bd5a25525d5fc9504758212c7049a90
MinhNhat-hub <Mun12082023@gmail.com>
2025-10-21 13:27:51 +0700

    Add files via upload

---
+++ BTTH2_PhamNhatMinh_245210830/BT04/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            cPhanSo PhanSoThuNhat = new cPhanSo();
+            cPhanSo PhanSoThuHai = new cPhanSo();
+
+            PhanSoThuNhat.NhapPhanSo();
+            PhanSoThuHai.NhapPhanSo();
+
+            cPhanSo Tong = PhanSoThuNhat + PhanSoThuHai;
+            Console.Write("Tong hai phan so la: ");
+            Tong.XuatPhanSo();
+
+            cPhanSo Hieu = PhanSoThuNhat - PhanSoThuHai;
+            Console.Write("Hieu hai phan so la: ");
+            Hieu.XuatPhanSo();
+
+            cPhanSo Tich = PhanSoThuNhat * PhanSoThuHai;
+            Console.Write("Tich hai phan so la: ");
+            Tich.XuatPhanSo();
+
+            try
+            {
+                cPhanSo Thuong = PhanSoThuNhat / PhanSoThuHai;
+                Console.Write("Thuong hai phan so la: ");
+                Thuong.XuatPhanSo();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Loi khong the chia: " + ex.Message);
+            }
+
+            int SoLuongPhanTu;
+            while (true)
+            {
+                Console.Write("Nhap so luong phan so trong mang: ");
+                if (int.TryParse(Console.ReadLine(), out int SL) && SL > 0)
+                {
+                    SoLuongPhanTu = SL;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("So luong khong phu hop, xin hay nhap lai");
+                }
+            }
+
+            cPhanSo[] DanhSachPhanSo = new cPhanSo[SoLuongPhanTu];
+
+            Console.WriteLine("Nhap danh sach phan so: ");
+
+            for(int i = 0;i < SoLuongPhanTu;i++)
+            {
+                DanhSachPhanSo[i] = new cPhanSo();
+                DanhSachPhanSo[i].NhapPhanSo();
+                DanhSachPhanSo[i].RutGon();
+            }
+
+            cPhanSo PhanSoLonNhat = DanhSachPhanSo[0];
+
+            foreach (cPhanSo PS  in DanhSachPhanSo)
+            {
+                if((PhanSoLonNhat - PS).getTuSo() < 0)
+                {
+                    PhanSoLonNhat = PS;
+                }
+            }
+
+            Console.Write("Phan so lon nhat cua mang la: ");
+            PhanSoLonNhat.XuatPhanSo();
+
+            Console.WriteLine("Sap xep cac phan so trong mang theo thu tu tang dan: ");
+
+            Array.Sort(DanhSachPhanSo);
+
+            foreach(cPhanSo PS in DanhSachPhanSo)
+            {
+                PS.XuatPhanSo();
+            }
+        }
+    }
+}
---
+++ BTTH2_PhamNhatMinh_245210830/BT04/cPhanSo.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT4
+{
+    class cPhanSo : IComparable<cPhanSo>
+    {
+        private int TuSo
+        {
+            get; set;
+        }
+        private int MauSo
+        {
+            get; set;
+        }
+
+        public cPhanSo(int Tu = 0, int Mau = 1)
+        {
+            TuSo = Tu;
+            MauSo = Mau;
+                
+        }
+
+        private int UCLN(int TuSo, int MauSo)
+        {
+            while (MauSo != 0)
+            {
+                int temp = MauSo;
+                MauSo = TuSo % MauSo;
+                TuSo = temp;
+            }
+            return TuSo;
+        }
+
+        public void NhapPhanSo()
+        {
+            while (true)
+            {
+                Console.Write("Nhap tu so: ");
+                if (int.TryParse(Console.ReadLine(), out int TS))
+                {
+                    TuSo = TS;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Ban nhap tu so khong phu hop, vui long nhap lai.");
+                }
+            }
+            while (true)
+            {
+                Console.Write("Nhap mau so: ");
+                if (int.TryParse(Console.ReadLine(), out int MS) && MS != 0)
+                {
+                    MauSo = MS;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Ban nhap mau so khong phu hop, vui long nhap lai.");
+                }
+            }
+        }
+
+        public cPhanSo RutGon()
+        {
+            int ucln = UCLN(Math.Abs(TuSo), Math.Abs(MauSo));
+            return new cPhanSo(TuSo / ucln, MauSo / ucln);
+        }
+
+        public static cPhanSo operator +(cPhanSo A, cPhanSo B)
+        {
+            return new cPhanSo(A.TuSo * B.MauSo + B.TuSo * A.MauSo, A.MauSo * B.MauSo).RutGon();
+        }
+
+        public static cPhanSo operator -(cPhanSo A, cPhanSo B)
+        {
+            return new cPhanSo(A.TuSo * B.MauSo - B.TuSo * A.MauSo, A.MauSo * B.MauSo).RutGon();
+        }
+
+        public static cPhanSo operator *(cPhanSo A, cPhanSo B)
+        {
+            return new cPhanSo(A.TuSo * B.TuSo, A.MauSo * B.MauSo).RutGon();
+        }
+
+        public static cPhanSo operator /(cPhanSo A, cPhanSo B)
+        {
+            if (B.TuSo == 0)
+                throw new ArgumentException("Khong the chia cho phan so co tu so bang 0 (phan so bang 0)");
+            return new cPhanSo(A.TuSo * B.MauSo, A.MauSo * B.TuSo).RutGon();
+        }
+
+        public void XuatPhanSo()
+        {
+            Console.WriteLine("{0}/{1}", TuSo, MauSo);
+        }
+
+        public int getTuSo()
+        {
+            return TuSo;
+        }
+
+        public int CompareTo(cPhanSo other)
+        {
+            double value1 = (double)TuSo / MauSo;
+            double value2 = (double)other.TuSo / other.MauSo;
+
+            if (value1 < value2) return -1;
+            if (value1 > value2) return 1;
+            return 0;
+        }
+    }
+}
