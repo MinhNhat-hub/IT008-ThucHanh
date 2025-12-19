c9b964defa454b8dd395251ff3ac78b2f0387aa6
MinhNhat-hub <Mun12082023@gmail.com>
2025-12-19 23:29:51 +0700

    Add files via upload

---
+++ BTTH3_PhamNhatMinh_245210830/BT1/Form1.cs
@@ -0,0 +1,40 @@
+﻿namespace BT1
+{
+    public partial class Form1 : Form
+    {
+        private ListBox listBoxEvents;
+
+        public Form1()
+        {
+            InitializeComponent();
+            listBoxEvents = new ListBox();
+            listBoxEvents.Dock = DockStyle.Fill;
+            Controls.Add(listBoxEvents);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBoxEvents.Items.Add("Load");
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            listBoxEvents.Items.Add("Shown");
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            listBoxEvents.Items.Add("Activated");
+        }
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            listBoxEvents.Items.Add("Deactivate");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            listBoxEvents.Items.Add("FormClosing");
+        }
+    }
+}
